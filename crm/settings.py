3708275d37fda955423e97663b87dc3466ed1730
Nikhil Mane <nmane1912@gmail.com>
2020-04-16 17:31:28 +0530

    test4

--- crm/settings.py
+++ crm/settings.py
@@ -150,10 +150,10 @@ STORAGE_TYPE = os.getenv('STORAGE_TYPE', 'normal')
 if STORAGE_TYPE == 'normal':
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     MEDIA_URL = '/media/'
-
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATIC_URL = '/static/'
-    STATICFILES_DIRS = (BASE_DIR + '/static',)
-    COMPRESS_ROOT = BASE_DIR + '/static/'
+    # STATICFILES_DIRS = (BASE_DIR + '/static',)
+    # COMPRESS_ROOT = BASE_DIR + '/static/'
 
 elif STORAGE_TYPE == 's3-storage':
 
